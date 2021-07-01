1d8e20118feec55df9e93e77bb4fe8f71e0cc2a6
Josue Lobo <darksiders20005@gmail.com>
2021-07-01 17:47:28 -0300

    refazendo a 10

--- src/zoo.js
+++ src/zoo.js
@@ -55,22 +55,98 @@ const calculateEntry = (entrants) => {
   return Object.keys(entrants).reduce((acc, cur) => acc + entrants[cur] * data.prices[cur], 0);
 };
 
-function getAnimalMap(options) {
-  // kkkkk da não bag dificil demais kkkkkkk.
+const optionUndefined = () => {
+  const acharAnimaisLocalization = (localization) => data.species.filter((specie) =>
+    specie.location === localization).map((specie2) => specie2.name);
+  const localizations = ['NE', 'NW', 'SE', 'SW'];
+  const resultado = {};
+  const tipoAnimais = localizations.map((localization) => acharAnimaisLocalization(localization));
+  localizations.forEach((key, index) => {
+    resultado[key] = tipoAnimais[index];
+  });
+  return resultado;
+};
+
+const animaisSexSortTrueFalse = (localization, index, sex = undefined, sorted = undefined) => {
+  let acharAnimaisSexTrueFalse;
+  if (sex === undefined) {
+    acharAnimaisSexTrueFalse = data.species.filter((specie) => specie.location === localization)
+      .map((specie2) => specie2.residents.map((name) => name.name))[index];
+  } else {
+    acharAnimaisSexTrueFalse = data.species.filter((specie) => specie.location === localization)
+      .map((specie2) => (specie2.residents.filter((specie) => specie.sex === sex)
+        .map((name) => name.name)))[index];
+  }
+  let ordemTrueFalse;
+  if (sorted === true) {
+    ordemTrueFalse = acharAnimaisSexTrueFalse.sort();
+  } else {
+    ordemTrueFalse = acharAnimaisSexTrueFalse;
+  }
+  return ordemTrueFalse;
+};
+
+const indexLocalization = (localizations) => data.species.map((localizatio) => localizatio.location)
+  .filter((loc) => loc === localizations);
+
+const animaisLocalization = (localization, index) => data.species.filter((specie) =>
+  specie.location === localization).map((specie2) => specie2.name)[index];
+
+const objetosTiposNames = (localization, index, sex, sorted) => {
+  const objeto = {
+    [animaisLocalization(localization, index)]:
+      animaisSexSortTrueFalse(localization, index, sex, sorted),
+  };
+  return objeto;
+};
+
+const arrayTiposNames = (localization, sex, sorted) => {
+  const array = [];
+  const localizations = indexLocalization(localization);
+  for (let index = 0; index < localizations.length; index += 1) {
+    array.push(objetosTiposNames(localization, index, sex, sorted));
+  }
+  return array;
+};
+
+const getAnimalMap = (options = undefined) => {
+  if (options === undefined || options.includeNames === undefined) {
+    return optionUndefined();
+  }
+  const { sex = undefined, sorted = undefined } = options;
+  const localizations = ['NE', 'NW', 'SE', 'SW'];
+  const objeto = {};
+  localizations.forEach((key) => {
+    objeto[key] = arrayTiposNames(key, sex, sorted);
+  });
+  return objeto;
+};
+
+function hoursConverter(hour) {
+  if (hour > 12) return `${(hour - 12)}pm`;
+  if (hour === 0) return '12pm';
+  return `${hour}am`;
+}
+
+function scheduleMessage(dayName, schedule) {
+  if (schedule[dayName].open !== schedule[dayName].close) {
+    return `Open from ${hoursConverter(schedule[dayName]
+      .open)} until ${hoursConverter(schedule[dayName].close)}`;
+  }
+  return 'CLOSED';
 }
 
 function getSchedule(dayName) {
-  const days = {
-    Tuesday: 'Open from 8am until 6pm',
-    Wednesday: 'Open from 8am until 6pm',
-    Thursday: 'Open from 10am until 8pm',
-    Friday: 'Open from 10am until 8pm',
-    Saturday: 'Open from 8am until 10pm',
-    Sunday: 'Open from 8am until 8pm',
-    Monday: 'CLOSED',
-  };
-  if (!dayName) return days;
-  return { [dayName]: days[dayName] };
+  const schedule = {};
+  const { hours } = data;
+  if (dayName) {
+    schedule[dayName] = scheduleMessage(dayName, hours);
+  } else {
+    Object.keys(hours).forEach((day) => {
+      schedule[day] = scheduleMessage(day, hours);
+    });
+  }
+  return schedule;
 }
 
 function getOldestFromFirstSpecies(id) {
